6bfcedc0d6a47d381daba8f831efb9fac23db674
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-12-19 17:56:33 +0100

    create '/codes' get all codes by user, create '/:id' delete code by id

--- routes/url.routes.js
+++ routes/url.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import db from "../db/index.js";
 import { urlsTable } from "../models/url.model.js";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { shortenPostRequestBodySchema } from "../validation/request.validation.js";
 import { ensureAuthenticated } from "../middlewares/auth.middleware.js";
 import {
@@ -42,6 +42,24 @@ router.post("/shorten", ensureAuthenticated, async function (req, res) {
     shortCode: result.shortCode,
     targetURL: result.targetURL,
   });
+});
+
+router.get("/codes", ensureAuthenticated, async function (req, res) {
+  const codes = await db
+    .select()
+    .from(urlsTable)
+    .where(eq(urlsTable.userId, req.user.id));
+
+  return res.json({ codes });
+});
+
+router.delete("/:id", ensureAuthenticated, async function (req, res) {
+  const id = req.params.id;
+  await db
+    .delete(urlsTable)
+    .where(and(eq(urlsTable.id, id), eq(urlsTable.userId, req.user.id)));
+
+    return res.status(200).json({deleted: true, "message": "URL deleted successfully"});
 });
 
 router.get("/:shortcode", async function (req, res) {
